447289ad9e6a514374d5ca6448c4659f51a99a0a
Jau <job_jausaihou@163.com>
2021-04-14 17:47:53 +0800

    小鹅学院初始化

---
+++ components/AppFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- 公共底 -->
+  <footer id="footer">
+    <section class="container">
+      <div class>
+        <h4 class="hLh30">
+          <span class="fsize18 f-fM c-999">友情链接</span>
+        </h4>
+        <ul class="of flink-list">
+          <li>
+            <a href="http://github.com/Jau-work/" title="个人github" target="_blank">个人github</a>
+          </li>
+        </ul>
+        <div class="clear"/>
+      </div>
+      <div class="b-foot">
+        <section class="fl col-7">
+          <section class="mr20">
+            <section class="b-f-link">
+              <a href="#" title="关于我们" target="_blank">关于我们</a>|
+              <a href="#" title="联系我们" target="_blank">联系我们</a>|
+              <a href="#" title="帮助中心" target="_blank">帮助中心</a>|
+              <a href="#" title="资源下载" target="_blank">资源下载</a>|
+              <span>Email：www.474665328@qq.com</span>
+            </section>
+            <section class="b-f-link mt10">
+              <span>©2021版权均归作者邱世豪所有</span>
+            </section>
+          </section>
+        </section>
+        <aside class="fl col-3 tac mt15">
+          <section class="gf-tx">
+            <span>
+              <img src="~/assets/img/wx-icon.png" alt>
+            </span>
+          </section>
+          <section class="gf-tx">
+            <span>
+              <img src="~/assets/img/wb-icon.png" alt>
+            </span>
+          </section>
+        </aside>
+        <div class="clear"/>
+      </div>
+    </section>
+  </footer>
+  <!-- /公共底 -->
+</template>
---
+++ pages/teacher/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(teacher, index) in items" :key="index">
+      <a href="#">{{ teacher.name }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import teacherApi from '~/api/teacher'
+export default {
+  // 服务器端渲染方案:
+  // 异步数据获取: 在前端服务器执行
+  asyncData() {
+    return teacherApi.getList().then(response => {
+      return {
+        items: response.data.items
+      }
+    })
+  }
+}
+</script>
